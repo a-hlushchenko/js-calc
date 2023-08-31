23502de5b4d325732bd695e0b7966c0021d6d53e
AntonHlushchenko <glantoshka@gmail.com>
2023-08-31 12:45:40 +0300

    upd route

--- src/route/calc.js
+++ src/route/calc.js
@@ -2,8 +2,8 @@ const express = require('express')
 const router = express.Router()
 
 router.get('/', function (req, res) {
-  res.render('calc', {
-    name: 'calc',
+  res.render('index', {
+    name: 'index',
     component: [],
     title: 'Калькулятор',
     
--- src/route/index.js
+++ src/route/index.js
@@ -1,15 +1,7 @@
-// Підключаємо роутер до бек-енду
 const express = require('express')
 const router = express.Router()
 
-// Підключіть файли роутів
-const test = require('./test')
 const calc = require('./calc')
-// Підключіть інші файли роутів, якщо є
+router.use('/', calc)
 
-// Об'єднайте файли роутів за потреби
-router.use('/calc', calc)
-// Використовуйте інші файли роутів, якщо є
-
-// Експортуємо глобальний роутер
 module.exports = router
